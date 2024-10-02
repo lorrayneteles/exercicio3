16ab0e8ce967e4f293528e8344f62fb7c7988e55
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-02 20:18:21 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,27 @@
+a = 2
+b = 5
+print("A soma de dois numeros é:")
+print (a + b)
+
+primeiro_nome = "Lorrayne "
+sobre_nome = "Teles "
+print ("o nome completo é:")
+print (primeiro_nome + sobre_nome)
+
+primeiro_nome = "Bruno Rafael "
+sobre_nome = "Moura"
+print ("O nome completo é:")
+print (primeiro_nome + sobre_nome)
+
+
+x = 9
+y = 10
+z = 5
+print("A média dos números é:")
+print( (x+y+z)/3)
+
+largura = 10
+altura = 3
+print("A área do retângulo é: ")
+print(largura*altura)
+
